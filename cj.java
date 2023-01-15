f623088ffae290592f29e6ae125f1b22bea51400
Your Name <you@example.com>
2023-01-15 12:51:20 -0400

    comit inicial

---
+++ cj.java
@@ -0,0 +1,4 @@
+⚠Para la versión universal, simplemente definir JavaSE-17 en java.
+configuration.runtimesno es suficiente para que vscode-java se inicie java.jdt.ls.java.
+home(o cualquiera de sus variables de entorno alternativas) todavía necesita apuntar a una ubicación 
+válida de JDK 17.
