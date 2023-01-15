13692cf6a3d21c38ab4b92980ac852eeada5fc73
Your Name <you@example.com>
2023-01-15 17:00:50 -0400

    commit inicia

--- cj.java
+++
@@ -1,4 +0,0 @@
-⚠Para la versión universal, simplemente definir JavaSE-17 en java.
-configuration.runtimesno es suficiente para que vscode-java se inicie java.jdt.ls.java.
-home(o cualquiera de sus variables de entorno alternativas) todavía necesita apuntar a una ubicación 
-válida de JDK 17.
